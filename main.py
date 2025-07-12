f887cb3311ffc8076f362f4824460e228d873454
GranBasti <bastian.mercover@gmail.com>
2025-07-12 11:14:06 -0400

    Proyecto final juego en pygame

--- main.py
+++ main.py
@@ -1,34 +1,16 @@
 import pygame
-
 import constantes
 from personaje import Personaje
 from weapon import Weapon
-import os
 from textos import DamageText
 from items import Item
 from mundo import Mundo
+import os
+import csv
 
 #Funciones
 
 #Escalar imagen
-
-
-
-pygame.init()
-
-#Variables de ventana
-#ancho = 800
-#alto = 600
-
-ventana = pygame.display.set_mode((constantes.ANCHO_VENTANA,
-                                   constantes.ALTO_VENTANA))
-
-#Cambiar el nombre de la ventana
-pygame.display.set_caption("Mi primer juego")
-
-#Fuentes
-font = pygame.font.Font("assets//fonts//monogram.ttf", 25)
-
 
 def escalar_img(image, scale):
     w = image.get_width()
@@ -47,8 +29,54 @@ def nombres_carpetas(directorio):
     return os.listdir(directorio)
 #print(nombres_carpetas("assets/images/characters/enemies"))
 
+pygame.init()
 
+#Variables de ventana
+#ancho = 800
+#alto = 600
 
+ventana = pygame.display.set_mode((constantes.ANCHO_VENTANA,
+                                   constantes.ALTO_VENTANA))
+
+#Cambiar el nombre de la ventana
+pygame.display.set_caption("Mi primer juego")
+
+#Variables
+posicion_pantalla = [0,0]
+nivel = 1
+
+#Fuentes
+font = pygame.font.Font("assets//fonts//monogram.ttf", 25)
+font_game_over = pygame.font.Font("assets//fonts//monogram.ttf", 100)
+font_reinicio = pygame.font.Font("assets//fonts//monogram.ttf", 30)
+font_inicio = pygame.font.Font("assets//fonts//monogram.ttf", 30)
+font_titulo = pygame.font.Font("assets//fonts//monogram.ttf", 30)
+
+game_over_text = font_game_over.render('Game Over', True, constantes.BLANCO)
+
+texto_boton_reinicio = font_reinicio.render("Reiniciar", True, constantes.NEGRO)
+
+boton_jugar = pygame.Rect(constantes.ANCHO_VENTANA/2-100,
+                          constantes.ALTO_VENTANA/2-50, 200, 50)
+
+boton_salir = pygame.Rect(constantes.ANCHO_VENTANA/2-100,
+                          constantes.ALTO_VENTANA/2-50, 200, 50)
+
+texto_boton_jugar = font_inicio.render("Jugar", True, constantes.NEGRO)
+texto_boton_salir = font_inicio.render("Salir", True, constantes.BLANCO)
+
+"""""
+def pantalla_inicio():
+    ventana.fill(constantes.COLOR_BG)
+    dibujar_texto("Mi primer juego", font_titulo, constantes.BLANCO,
+                  constantes.ANCHO_VENTANA/2-200,
+                  constantes.ALTO_VENTANA/2-200)
+    pygame.draw.rect(ventana, constantes.AMARILLO, boton_jugar)
+    pygame.draw.rect(ventana, constantes.ROJO, boton_salir)
+    ventana.blit(texto_boton_jugar, (boton_jugar.x+50, boton_jugar.y+10))
+    ventana.blit(texto_boton_salir, (boton_salir.x + 50, boton_salir.y + 10))
+    pygame.display.update()
+"""""
 #Importar imagenes
 #Energía
 corazon_vacio = pygame.image.load("assets//images//items//Heart_3.png").convert_alpha()
@@ -101,7 +129,7 @@ imagen_balas = escalar_img(imagen_balas, constantes.SCALA_ARMA)
 #Cargar imagenes del mundo
 tile_list = []
 for x in range(constantes.TILE_TYPES):
-    tile_image = pygame.image.load(f"assets//images//tiles//Rock_{x+1}.png")
+    tile_image = pygame.image.load(f"assets//images//tiles//Rock_{x + 1}.png")
     tile_image = pygame.transform.scale(tile_image, (constantes.TILE_SIZE, constantes.TILE_SIZE))
     tile_list.append(tile_image)
 
@@ -138,15 +166,37 @@ def vida_jugador():
             ventana.blit(corazon_vacio, (5+i*50,5))
 
 world_data = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 ]
+
+"""""
+for fila in range(constantes.FILAS):
+    filas = [5] * constantes.COLUMNAS
+    world_data.append(filas)
+
+#cargar el archivo con el nivel
+with open("niveles//dungeon.csv", newline=' ') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for x, fila in enumerate(reader):
+        for y, columna in enumerate(fila):
+            world_data[x][y] = int(columna)
+"""""
 
 world = Mundo()
 world.process_data(world_data, tile_list)
@@ -158,12 +208,12 @@ def dibujar_grid():
         pygame.draw.line(ventana, constantes.BLANCO, (0, x * constantes.TILE_SIZE), (constantes.ANCHO_VENTANA, x*constantes.TILE_SIZE))
 
 #Crear un jugador de la clase Personaje
-jugador = Personaje(50,50, animaciones, 70)
+jugador = Personaje(10,10, animaciones, 70, 1)
 
 #Crear un enemigo de la clase Personaje
-demon = Personaje(200, 200, animaciones_enemigos[0], 100)
-demon_2 = Personaje(400, 400, animaciones_enemigos[0], 100)
-freezer = Personaje(40, 40, animaciones_enemigos[1], 200)
+demon = Personaje(200, 200, animaciones_enemigos[0], 100, 2)
+demon_2 = Personaje(400, 400, animaciones_enemigos[0], 100, 2)
+freezer = Personaje(300, 100, animaciones_enemigos[1], 200, 2)
 
 
 #Crear lista de enemigos
@@ -182,11 +232,13 @@ grupo_damage_text = pygame.sprite.Group()
 grupo_balas = pygame.sprite.Group()
 grupo_items = pygame.sprite.Group()
 
-coin = Item(350, 25, 0, coin_images)
-potion = Item(380, 55, 1, [pocion_turquesa])
+coin = Item(350, 350, 0, coin_images)
+coin_1 = Item(550, 250, 0, coin_images)
+potion = Item(380, 500, 1, [pocion_turquesa])
+potion_1 = Item(470, 230, 1, [pocion_turquesa])
 
-grupo_items.add(coin)
-grupo_items.add(potion)
+grupo_items.add(coin, coin_1)
+grupo_items.add(potion, potion_1)
 
 
 #temporal y borrar
@@ -203,95 +255,118 @@ mover_derecha = False
 reloj = pygame.time.Clock()
 
 
+mostrar_inicio = True
 #Variable
 run = True
 
 #LOOP para mantener la ventana abierta
 while run:
 
-    #60 FPS Frame Rate
+        #60 FPS Frame Rate
     reloj.tick(constantes.FPS)
 
     ventana.fill(constantes.COLOR_BG)
 
-    dibujar_grid()
-    #Calcular el movimiento del jugador
-    delta_x = 0
-    delta_y = 0
-
-    if mover_derecha == True:
-        delta_x = constantes.VELOCIDAD
-    if mover_izquierda == True:
-        delta_x = -constantes.VELOCIDAD
-    if mover_arriba == True:
-        delta_y = -constantes.VELOCIDAD
-    if mover_abajo == True:
-        delta_y = constantes.VELOCIDAD
+    if jugador.vivo:
 
 
-    #Mover el jugador
-    jugador.movimiento(delta_x, delta_y)
+            #Calcular el movimiento del jugador
+        delta_x = 0
+        delta_y = 0
 
-    #Actualiza el estado jugador
-    jugador.update()
-
-    #Actualiza el estado del enemigo
-    for ene in lista_enemigos:
-        ene.update()
-        #print (ene.energia)
-
-    #Actualizar el estado del arma
-    bala = shuriken.update(jugador)
-    if bala:
-        grupo_balas.add(bala)
-    for bala in grupo_balas:
-        damage, pos_damage = bala.update(lista_enemigos)
-        if damage:
-            damage_text = DamageText(pos_damage.centerx, pos_damage.centery, str(damage), font, constantes.ROJO)
-            grupo_damage_text.add(damage_text)
-
-    #Actualizar daño
-    grupo_damage_text.update()
+        if mover_derecha == True:
+            delta_x = constantes.VELOCIDAD
+        if mover_izquierda == True:
+            delta_x = -constantes.VELOCIDAD
+        if mover_arriba == True:
+            delta_y = -constantes.VELOCIDAD
+        if mover_abajo == True:
+            delta_y = constantes.VELOCIDAD
 
 
-    #Actualizar items
-    grupo_items.update(jugador)
+            #Mover el jugador
+        posicion_pantalla = jugador.movimiento(delta_x, delta_y)
+        print (posicion_pantalla)
 
-    #Dibujar mundo
+        world.update(posicion_pantalla)
+
+            #Actualiza el estado jugador
+        jugador.update()
+
+            #Actualiza el estado del enemigo
+        for ene in lista_enemigos:
+            ene.update()
+                #print (ene.energia)
+
+            #Actualizar el estado del arma
+        bala = shuriken.update(jugador)
+        if bala:
+            grupo_balas.add(bala)
+        for bala in grupo_balas:
+            damage, pos_damage = bala.update(lista_enemigos)
+            if damage:
+                damage_text = DamageText(pos_damage.centerx, pos_damage.centery, str(damage), font, constantes.ROJO)
+                grupo_damage_text.add(damage_text)
+
+        #Actualizar daño
+        grupo_damage_text.update(posicion_pantalla)
+
+            #Actualizar items
+        grupo_items.update(posicion_pantalla, jugador)
+
+        #Dibujar mundo
     world.draw(ventana)
 
-    #print(grupo_balas)
+        #print(grupo_balas)
 
-    #Dibujar al jugador
+        #Dibujar al jugador
     jugador.dibujar(ventana)
 
-    #Dibujar al enemigos
+        #Dibujar al enemigos
     for ene in lista_enemigos:
-        ene.dibujar(ventana)
+        if ene.energia == 0:
+            lista_enemigos.remove(ene)
+        if ene.energia > 0:
+            ene.enemigos(jugador, posicion_pantalla)
+            ene.dibujar(ventana)
 
-    #Dibujar el arma
+        #Dibujar el arma
     shuriken.dibujar(ventana)
 
-    #Dibujar balas
+        #Dibujar balas
     for bala in grupo_balas:
         bala.dibujar(ventana)
 
-    #Dibujar los corazones
+        #Dibujar los corazones
     vida_jugador()
 
-    #Dibujar textos
+        #Dibujar textos
     grupo_damage_text.draw(ventana)
     dibujar_texto(f"Score: {jugador.score}", font, (255,255,0), 700, 5)
+        #nivel
+    dibujar_texto(f"Nivel: " + str(nivel), font, constantes.BLANCO, constantes.ANCHO_VENTANA/2, 5)
 
-    # dibujar items
+        # dibujar items
     grupo_items.draw(ventana)
 
+    if jugador.vivo == False:
+        ventana.fill(constantes.ROJO_OSCURO)
+        text_rect = game_over_text.get_rect(center = (constantes.ANCHO_VENTANA/2,
+                                                          constantes.ALTO_VENTANA/2))
+        ventana.blit(game_over_text, text_rect)
+
+        boton_reinicio = pygame.Rect(constantes.ANCHO_VENTANA/2-100,
+                                         constantes.ALTO_VENTANA/2+100, 200, 50)
+        pygame.draw.rect(ventana, constantes.AMARILLO, boton_reinicio)
+        ventana.blit(texto_boton_reinicio,
+                         (boton_reinicio.x+50, boton_reinicio.y + 10))
+
     for event in pygame.event.get():
-        #Para cerrar el juego
+            #Para cerrar el juego
         if event.type == pygame.QUIT:
             run = False
 
-        #Mover jugador
+            #Mover jugador
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
                 mover_izquierda = True
@@ -303,7 +378,7 @@ while run:
                 mover_derecha = True
 
 
-        #para cuando se suelta la tecla
+            #para cuando se suelta la tecla
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_a:
                 mover_izquierda = False
@@ -314,9 +389,23 @@ while run:
             if event.key == pygame.K_d:
                 mover_derecha = False
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if boton_reinicio.collidepoint(event.pos) and not jugador.vivo:
+                jugador.vivo = True
+                jugador.energia = 100
+                jugador.score = 0
+                nivel = 1
+                world = Mundo()
+                world.process_data(world_data, tile_list)
+                lista_enemigos = []
+                demon = Personaje(200, 200, animaciones_enemigos[0], 100, 2)
+                demon_2 = Personaje(400, 400, animaciones_enemigos[0], 100, 2)
+                freezer = Personaje(300, 100, animaciones_enemigos[1], 200, 2)
+                lista_enemigos.append(demon)
+                lista_enemigos.append(demon_2)
+                lista_enemigos.append(freezer)
 
-
-    #Para actualizar la ventana
+        #Para actualizar la ventana
     pygame.display.update()
 
 pygame.quit()
--- personaje.py
+++ personaje.py
@@ -1,11 +1,9 @@
 import pygame
-from pygame.examples.cursors import image
-
 import constantes
-
+import math
 
 class Personaje():
-    def __init__(self, x, y, animaciones, energia):
+    def __init__(self, x, y, animaciones, energia, tipo):
         self.score = 0
         self.energia = energia
         self.vivo = True
@@ -19,8 +17,16 @@ class Personaje():
         self.update_time = pygame.time.get_ticks()
         self.forma = self.image.get_rect()
         self.forma.center = (x,y)
+        self.tipo = tipo
+        self.golpe = False
+        self.ultimo_golpe = pygame.time.get_ticks()
+        self.golpe = False
+        self.ultimo_golpe = pygame.time.get_ticks()
+
 
     def movimiento (self, delta_x, delta_y):
+        posicion_pantalla = [0,0]
+        nivel_completado = False
 
        #Para Voltear el Asset
         if delta_x < 0:
@@ -31,12 +37,66 @@ class Personaje():
         self.forma.x = self.forma.x + delta_x
         self.forma.y = self.forma.y + delta_y
 
+        #Lógica solo aplica al jugador y no enemigos
+        if self.tipo == 1:
+            #Actualizar la pantalla basada la posición del jugador
+            #Mover la cámara izquierda a derecha
+            if self.forma.right > (constantes.ANCHO_VENTANA - constantes.LIMITE_PANTALLA):
+                posicion_pantalla[0] = (constantes.ANCHO_VENTANA - constantes.LIMITE_PANTALLA) - self.forma.right
+                self.forma.right = constantes.ANCHO_VENTANA - constantes.LIMITE_PANTALLA
+            if self.forma.left < constantes.LIMITE_PANTALLA:
+                posicion_pantalla[0] = constantes.LIMITE_PANTALLA - self.forma.left
+                self.forma.left = constantes.LIMITE_PANTALLA
+
+            if self.forma.bottom > (constantes.ALTO_VENTANA - constantes.LIMITE_PANTALLA):
+                posicion_pantalla[1] = (constantes.ALTO_VENTANA - constantes.LIMITE_PANTALLA) - self.forma.bottom
+                self.forma.bottom = constantes.ALTO_VENTANA - constantes.LIMITE_PANTALLA
+            if self.forma.top < constantes.LIMITE_PANTALLA:
+                posicion_pantalla[1] = constantes.LIMITE_PANTALLA - self.forma.top
+                self.forma.top = constantes.LIMITE_PANTALLA
+        return posicion_pantalla
+
+    def enemigos (self, jugador, posicion_pantalla):
+        ene_dx = 0
+        ene_dy = 0
+
+        #Reposicion de enemigos basado en la posición de la pantalla o c´mara
+        self.forma.x += posicion_pantalla[0]
+        self.forma.y += posicion_pantalla[1]
+
+        #Distancia con el jugador
+        distancia = math.sqrt(((self.forma.centerx - jugador.forma.centerx)**2) +
+                        ((self.forma.centery - jugador.forma.centery)**2))
+        if distancia < constantes.RANGO:
+            if self.forma.centerx > jugador.forma.centerx:
+                ene_dx = -constantes.VELOCIDAD_ENEMIGO
+            if self.forma.centerx < jugador.forma.centerx:
+                ene_dx = constantes.VELOCIDAD_ENEMIGO
+            if self.forma.centery > jugador.forma.centery:
+                ene_dy = -constantes.VELOCIDAD_ENEMIGO
+            if self.forma.centery < jugador.forma.centery:
+                ene_dy = constantes.VELOCIDAD_ENEMIGO
+        self.movimiento(ene_dx,ene_dy)
+
+        #Atacar al jugador
+        if distancia < constantes.RANGO_ATAQUE and jugador.golpe == False:
+            jugador.energia -= 10
+            jugador.golpe = True
+            jugador.ultimo_golpe = pygame.time.get_ticks()
+
     #Metodo en milisegundos de cooldown
     def update(self):
         #Comprobar si el personaje ha muerto
         if self.energia <= 0:
             self.energia = 0
             self.vivo = False
+
+        #timer para poder volver a recibir daño
+        golpe_cooldown = 1000
+        if self.tipo == 1:
+            if self.golpe == True:
+                if pygame.time.get_ticks() - self.ultimo_golpe > golpe_cooldown:
+                    self.golpe = False
 
         cooldown_animacion = 100
         self.image = self.animaciones[self.frame_index]
